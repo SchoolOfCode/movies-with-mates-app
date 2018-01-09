f7e7af21be38f46fecdb3b85777c90761485073b
Petekachu <peteryu.uk@gmail.com>
2018-01-09 16:34:53 +0000

    usernames on tickets now centred + avatars

--- src/components/Ticket/index.js
+++ src/components/Ticket/index.js
@@ -12,8 +12,8 @@ const Ticket = props => (
 	<Link to={`/movies/${props.movie}`}>
 		<div id="ticket" style={{ margin: 0 }}>
 			<img style={{position: "relative", left: "-30%", top:"6vh"}} src={props.user.picture} id="img" />
-			<div className="userName" style={{position:"relative", width: "15vw", height: "20%", top: "15%", left: "12%", margin: "0"}}>
-				<p style={{margin: "0"}} id="name">{props.user.name}</p>
+			<div className="userName" style={{position:"relative", width: "22vw", height: "15%", top: "28%", left: "7%", margin: "0"}}>
+				<p style={{ position:"relative", top: "0", margin: "0 auto", textAlign: "center"}} id="name">{props.user.name}</p>
 			</div>
 			<p id="info" style={{ bottom:"25px", top:"3px", left: '37%', fontSize: '1.32em', width: '51vw' }}>
 				<br />
