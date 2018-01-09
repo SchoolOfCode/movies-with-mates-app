5374e23d97c2da2d001b1f6fa2fe3079199189b6
MogHirsch93 <morganhirsch93@hotmail.co.uk>
2018-01-09 15:40:14 +0000

    Post movie has new logos too

--- src/components/Clock/index.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Clock = props => {
-  return <img style={props.style} src="clock-icon.svg" />;
-};
-
-export default Clock;
--- src/components/MovieLogo/index.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const MovieLogo = props => {
-  return <img style={props.style} src="movie-clapper-icon.svg" />;
-};
-
-export default MovieLogo;
