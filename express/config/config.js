fef92c5766973a5826779dacbe8950ef88aca182
leighwhite96 <leigh_white@live.co.uk>
2018-01-11 15:02:48 +0000

    we can email users when they post an event

--- express/config/config.js
+++ express/config/config.js
@@ -1,11 +1,17 @@
 const config = {
-
   dbProtocol: process.env.DB_PROTOCOL,
   dbHost: process.env.DB_HOST,
   dbPort: process.env.DB_PORT,
   dbName: process.env.DB_NAME,
   dbUser: process.env.DB_USER,
-  dbPass: process.env.DB_PASS
+  dbPass: process.env.DB_PASS,
+  email: {
+    service: process.env.EMAIL_SERVICE,
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS
+    }
+  }
 };
 
 module.exports = config;
