25b8d3ac0a84c02515b3ad66324f84bdce46765a
Petekachu <peteryu.uk@gmail.com>
2018-01-14 14:26:22 +0000

    logo as one svg

--- src/components/LandingPage/index.js
+++ src/components/LandingPage/index.js
@@ -1,50 +1,121 @@
-import React from "react";
+import React from 'react';
 
-import Logo from "../Logo";
-import TagLine from "../TagLine";
-import FBLoginButton from "../FBLoginButton";
-import LoginEmailButton from "../LoginEmailButton";
-import RegisterLink from "../RegisterLink";
+import Logo from '../Logo';
+import TagLine from '../TagLine';
+import FBLoginButton from '../FBLoginButton';
+import LoginEmailButton from '../LoginEmailButton';
+import RegisterLink from '../RegisterLink';
 
-import "./LandingPage.css";
+import './LandingPage.css';
 
 const LandingPage = props => {
-
   return (
     <div>
-      <div className="landingAnimation" style={{ position: "absolute", height: "100vh", width: "100vw", backgroundColor:"white", zIndex:"30"}}>
-        <div style={{position: "relative", top: "25vh"}}>
+      <div
+        className="landingAnimation"
+        style={{
+          position: 'absolute',
+          height: '100vh',
+          width: '100vw',
+          backgroundColor: 'white',
+          zIndex: '30'
+        }}
+      >
+        <div style={{ position: 'relative', top: '25vh' }}>
           <img
-            src={"/ticket-v5.svg"}
-            style={{zIndex: "10", position: "relative", top: "13vh", left:"23%",height: "50%", width: "50%", transform:"rotate(-45deg)"}}
+            src={'/ticket-v5.svg'}
+            style={{
+              zIndex: '10',
+              position: 'relative',
+              top: '13vh',
+              left: '23%',
+              height: '50%',
+              width: '50%',
+              transform: 'rotate(-45deg)'
+            }}
           />
           <img
-            src={"/ticket-v6.svg"}
-            style={{zIndex:"20", position: "relative", top: "18vh", left:"-23%", height: "50%", width: "50%"}}
+            src={'/ticket-v6.svg'}
+            style={{
+              zIndex: '20',
+              position: 'relative',
+              top: '18vh',
+              left: '-23%',
+              height: '50%',
+              width: '50%'
+            }}
           />
-          <p style= {{color: "white", position: "absolute", top: "129px", left: "112px", fontSize: "5.75em", zIndex:"25"}}>M W M</p>
-          <div style={{position: "relative", top: "20vh"}}>
-            <h2 style={{position: "relative", top: "1vh", left: "8px", fontSize: "2em", margin:"0 auto", width: "50vw"}}>Movies With Mates</h2>
+          <p
+            style={{
+              color: 'white',
+              position: 'absolute',
+              top: '129px',
+              left: '112px',
+              fontSize: '5.75em',
+              zIndex: '25'
+            }}
+          >
+            M W M
+          </p>
+          <div style={{ position: 'relative', top: '20vh' }}>
+            <h2
+              style={{
+                position: 'relative',
+                top: '1vh',
+                left: '8px',
+                fontSize: '2em',
+                margin: '0 auto',
+                width: '50vw'
+              }}
+            >
+              Movies With Mates
+            </h2>
           </div>
         </div>
       </div>
-      <div style={{width: "100vw", backgroundColor:"black"}}>
-        <img className="background" src={"/cinema-room.jpg"}/>
+      <div style={{ width: '100vw', backgroundColor: 'black' }}>
+        <img className="background" src={'/cinema-room.jpg'} />
         <div className="mwmLogo">
           <img
-            src={"/ticket-v5.svg"}
-            style={{zIndex: "1", position: "relative", top: "13vh", left:"23%",height: "50%", width: "50%", transform:"rotate(-45deg)"}}
+            src={'/ticket-v5.svg'}
+            style={{
+              zIndex: '1',
+              position: 'relative',
+              top: '13vh',
+              left: '23%',
+              height: '50%',
+              width: '50%',
+              transform: 'rotate(-45deg)'
+            }}
           />
           <img
-            src={"/ticket-v6.svg"}
-            style={{zIndex:"2", position: "relative", top: "18vh", left:"-23%", height: "50%", width: "50%"}}
+            src={'/ticket-v6.svg'}
+            style={{
+              zIndex: '2',
+              position: 'relative',
+              top: '18vh',
+              left: '-23%',
+              height: '50%',
+              width: '50%'
+            }}
           />
         </div>
-        <div style={{zIndex: "2"}}>
-          <p style= {{color: "white", position: "absolute", top: "129px", left: "112px", fontSize: "5.75em", zIndex:"25"}}>M W M</p>
+        <div style={{ zIndex: '2' }}>
+          <p
+            style={{
+              color: 'white',
+              position: 'absolute',
+              top: '129px',
+              left: '112px',
+              fontSize: '5.75em',
+              zIndex: '25'
+            }}
+          >
+            M W M
+          </p>
         </div>
         <div className="tagLine">
-          <TagLine style={{ fontFamily: "Roboto"}} />
+          <TagLine style={{ fontFamily: 'Roboto' }} />
         </div>
         <div className="loginButtons">
           <FBLoginButton history={props.history} />
